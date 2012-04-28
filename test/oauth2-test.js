8ad79d81ca576ef3db936ec439ad432e35f1b866
Jared Hanson <jaredhanson@gmail.com>
2012-04-27 23:56:16 -0700

    Wrap error for informative messages.

--- test/oauth2-test.js
+++ test/oauth2-test.js
@@ -139,6 +139,9 @@ vows.describe('GoogleStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
